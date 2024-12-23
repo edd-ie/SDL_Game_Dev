dc89bd4e3c1d1a620177ed78dd3579511100cef0
edd-ie <ogendoeddie@gmail.com>
2024-12-23 19:33:24 -0400

    Sprite animations and image flipping

--- Game.cpp
+++ Game.cpp
@@ -3,6 +3,7 @@
 //
 #include "Game.h"
 #include<iostream>
+#include<SDL_image.h>
 
 Game::Game() {
     running = false;
@@ -29,10 +30,10 @@ void Game::init(const char* title, int xPos, int yPos, int width,
             if(gRenderer != nullptr)
             {
                 std::cout << "renderer creation success\n";
-                SDL_SetRenderDrawColor(gRenderer,255,255,255,255);
+                SDL_SetRenderDrawColor(gRenderer,0,0,0,255);
                 running = true;
 
-                if (SDL_Surface* pTempSurface = SDL_LoadBMP("resources/rider.bmp");
+                if (SDL_Surface* pTempSurface =  IMG_Load("resources/pack/Effect Block-Sheet.png");
                     pTempSurface == nullptr) std::cout << "surface fail\n";
                 else {
                     gTexture = SDL_CreateTextureFromSurface(gRenderer, pTempSurface);
@@ -43,8 +44,10 @@ void Game::init(const char* title, int xPos, int yPos, int width,
 
                     destinationRectangle.x = sourceRectangle.x = 0;
                     destinationRectangle.y = sourceRectangle.y = 0;
-                    destinationRectangle.w = sourceRectangle.w;
-                    destinationRectangle.h = sourceRectangle.h;
+                    sourceRectangle.w = 32;
+                    sourceRectangle.h = 32;
+                    destinationRectangle.w = 50;
+                    destinationRectangle.h = 50;
                 }
 
             }
@@ -58,13 +61,15 @@ void Game::init(const char* title, int xPos, int yPos, int width,
 void Game::render() {
     SDL_RenderClear(gRenderer); // clear the renderer to the draw color
 
-    SDL_RenderCopy(gRenderer, gTexture, &sourceRectangle,
-    &destinationRectangle);
+    SDL_RenderCopyEx(gRenderer, gTexture, &sourceRectangle,
+        &destinationRectangle, 0, 0, SDL_FLIP_HORIZONTAL);
 
     SDL_RenderPresent(gRenderer); // draw to the screen
 }
 
-void Game::update(){}
+void Game::update() {
+    sourceRectangle.x = 32 * static_cast<int>((SDL_GetTicks() / 200) % 5);
+}
 
 void Game::handleEvents() {
     SDL_Event event;
